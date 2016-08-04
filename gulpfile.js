34ef4289442dde0680618f0dd8b5ed3d0601c02e
leandroargentiero <argentieroleo@gmail.com>
2016-08-04 16:07:55 +0200

    New task for minifying images

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
 	concatCSS = require('gulp-concat-css'),
 	minifyCSS = require('gulp-minify-css'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	imageMIN = require('gulp-imagemin');
+	
 
 gulp.task('sass', function(){
 	return gulp.src('./sass/*.scss')
@@ -10,8 +12,14 @@ gulp.task('sass', function(){
 	.pipe(gulp.dest('./build/css'));
 });
 
+gulp.task('imageMIN', function(){
+	return gulp.src('./images/*')
+	.pipe(imageMIN())
+	.pipe(gulp.dest('./build/img'));
+});
+
 gulp.task('watch', function(){
 	gulp.watch('./sass/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'imageMIN','watch']);
