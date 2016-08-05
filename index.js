70cf6746d849d4824099cadbb53605a3a1401e5c
leandroargentiero <argentieroleo@gmail.com>
2016-08-05 13:07:14 +0200

    Updated routing and io.emit to all works

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.use('/build', express.static(__dirname + '/build'));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/scoreboard.html');
+	res.sendFile(__dirname + '/build/scoreboard.html');
 });
+
+app.get('/scoreboard', function(req, res){
+	res.sendFile(__dirname + '/build/scoreboard.html');
+});
+
+app.get('/admin', function(req, res){
+	res.sendFile(__dirname + '/build/admin.html');
+});
+
+io.on('connection', function(socket){
+	socket.on('selector-team1', function(msg){
+		io.emit('selector-team1', msg);
+		console.log(msg);
+	});
+	
+	socket.on('selector-team2', function(msg){
+		io.emit('selector-team2', msg);
+		console.log(msg);
+	});
+});
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
